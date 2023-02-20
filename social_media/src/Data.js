98ccfc6f32f3c94257c6b8b2031a1b109cdbe114
RAGHURAJ SINGH RAJPOOT <114226990+Raghuraj361@users.noreply.github.com>
2023-02-20 18:41:01 +0530

    Create Data.js

---
+++ social_media/src/Data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        id : 1,
+        Image : "https://cdn.bhdw.net/im/doll-couple-wallpaper-3497_w635.webp",
+        Description : "friendship forever",
+        PostedBy : "Rishav Raj",
+        DatePoster : "January 14, 2023",
+        UserImg : "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+        id : 2,
+        Image : "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&w=600",
+        Description : "Red blossom flower",
+        PostedBy : "Ankush pratap",
+        DatePoster : "January 15, 2023",
+        UserImg : "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+        id : 3,
+        Image : "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?auto=compress&cs=tinysrgb&w=600",
+        Description : "king of jungle",
+        PostedBy : "Shraddha tiwari",
+        DatePoster : "January 16, 2023",
+        UserImg : "https://avatars.githubusercontent.com/u/114010000?v=4",
+    },
+    {
+        id : 4,
+        Image : "https://images.pexels.com/photos/1209843/pexels-photo-1209843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        Description : "holi fastival",
+        PostedBy : "Arun sharma",
+        DatePoster : "January 17, 2023",
+        UserImg : "https://images.pexels.com/photos/716411/pexels-photo-716411.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+        id : 5,
+        Image : "https://www.imagesjunction.com/images/img/Cute_cat_images.jpg",
+        Description : "cat",
+        PostedBy : "Shivansh Shivansh",
+        DatePoster : "January 18, 2023",
+        UserImg : "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+]
+
+export default data;
