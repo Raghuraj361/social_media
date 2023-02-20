5bd8b577cb1e1e1ab8c8a4c59665c49edb542245
RAGHURAJ SINGH RAJPOOT <114226990+Raghuraj361@users.noreply.github.com>
2023-02-20 18:44:31 +0530

    Update Post.jsx

--- social_media/src/components/Post.jsx
+++ social_media/src/components/Post.jsx
@@ -10,14 +10,14 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
-const Post = () => {
+const Post = ({id, Image, Description, PostedBy, DatePoster, UserImg}) => {
   return (
     <Card sx={{ margin: 5 }}>
       <CardHeader
         avatar={
           <Avatar 
               sx={{ width: 30, height: 30 }}
-              src="https://media.licdn.com/dms/image/C4D03AQGhAiaw2SDIAQ/profile-displayphoto-shrink_800_800/0/1663955447753?e=1681948800&v=beta&t=s4GWeHR5by6fzjIeINHt9BBDKsL8XFSQDBFF8EJJjWs"
+              src={UserImg}
           />
         }
         action={
@@ -25,20 +25,18 @@ const Post = () => {
             <MoreVert />
           </IconButton>
         }
-        title="Raghuraj singh"
-        subheader="January 14, 2023"
+        title={PostedBy}
+        subheader={DatePoster}
       />
       <CardMedia
         component="img"
         height="20%"
-        image="https://cdn.bhdw.net/im/doll-couple-wallpaper-3497_w635.webp"
+        image={Image}
         alt="Paella dish"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {Description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -48,7 +46,7 @@ const Post = () => {
             checkedIcon={<Favorite sx={{ color: "red" }} />}
           />
         </IconButton>
-        <IconButton aria-label="share">
+        <IconButton aria-label="comment">
           <Comment />
         </IconButton>
       </CardActions>
